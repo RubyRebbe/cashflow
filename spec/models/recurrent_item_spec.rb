abd6819be8da1eb630798f484d2c9f10e1f92828
Tsvi Bar-David <tsvi.bardavid@gmail.com>
2017-12-06 07:00:17 -0800

    small fixes to kashflow views

--- spec/models/recurrent_item_spec.rb
+++ spec/models/recurrent_item_spec.rb
@@ -1,3 +1,7 @@
 describe "Recurrent Item" do
-  it "can define a recurrent item"
+  it "has a start_date"
+	it "has a end_date"
+	it "start_date and end_date must fall within the same year"
+	it "associated items share same day of month - start_date day"
+	it "destroying it destroys the associated items"
 end
